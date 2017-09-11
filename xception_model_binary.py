6dcd8aa964ca85c1c5e8694dad9448a69b194c60
Ubuntu <ubuntu@ip-172-31-10-67.us-west-1.compute.internal>
2017-09-11 17:57:00 +0000

    commiting stuff so that I can try re-running multilabel analysis on the other computer.

--- xception_model_binary.py
+++ xception_model_binary.py
@@ -15,7 +15,7 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential, load_model
 from keras.layers import Dropout, Flatten, Dense,GlobalAveragePooling2D
 from keras import applications
-from keras import backend as K
+#from keras import backend as K
 from keras import callbacks
 
 
@@ -49,34 +49,6 @@ available_train_files % 8
 # Useful metrics for model evaluation
 
 # In[16]:
-
-def precision(y_true, y_pred):
-    """Precision metric.
-
-    Only computes a batch-wise average of precision.
-
-    Computes the precision, a metric for multi-label classification of
-    how many selected items are relevant.
-    """
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-    precision = true_positives / (predicted_positives + K.epsilon())
-    return precision
-
-
-def recall(y_true, y_pred):
-    """Recall metric.
-
-    Only computes a batch-wise average of recall.
-
-    Computes the recall, a metric for multi-label classification of
-    how many relevant items are selected.
-    """
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-    recall = true_positives / (possible_positives + K.epsilon())
-    return recall
-
 
 # Create Generators
 
@@ -180,6 +152,12 @@ for layer in watch_model.layers[0].layers[-3:]:
     layer.trainable = True
 
 epochs = 50
+
+
+watch_model.compile(
+    loss = "binary_crossentropy", 
+    optimizer='sgd', 
+    metrics=["accuracy"])
 
 
 cb2 = [callbacks.ModelCheckpoint(
