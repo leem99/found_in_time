56a86c819c23ac60dce75ecee8e25da93969dbbd
Ubuntu <ubuntu@ip-172-31-10-67.us-west-1.compute.internal>
2017-09-13 20:15:43 +0000

    watch gender classifier up and running.

--- xception_model_binary.py
+++ xception_model_binary.py
@@ -3,7 +3,7 @@
 
 # ## Create "Model Topper" using example
 
-# In[23]:
+# In[4]:
 
 
 import os
@@ -19,7 +19,7 @@ from keras import applications
 from keras import callbacks
 
 
-# In[14]:
+# In[5]:
 
 # dimensions of our images.
 img_width, img_height = 299, 299
@@ -28,9 +28,8 @@ img_width, img_height = 299, 299
 img_width, img_height = 299, 299
 n_features = 1
 
-top_model_weights_path = 'bottleneck_xception_model.h5'
-train_data_dir = 'multi_class_testing/train/'
-validation_data_dir = 'multi_class_testing/test/'
+train_data_dir = 'binary_gender_masked/train/'
+validation_data_dir = 'binary_gender_masked/test/'
 
 available_train_files = len(os.listdir(train_data_dir + 'female/'))     + len(os.listdir(train_data_dir + 'male/'))
 available_test_files = len(os.listdir(validation_data_dir + 'female/'))     + len(os.listdir(validation_data_dir+'male/'))
@@ -41,18 +40,46 @@ nb_validation_samples = available_test_files - available_test_files % 8
 batch_size = 8
 
 
-# In[15]:
+# In[6]:
 
 available_train_files % 8
 
 
 # Useful metrics for model evaluation
 
-# In[16]:
+# In[7]:
+
+# def precision(y_true, y_pred):
+#     """Precision metric.
+
+#     Only computes a batch-wise average of precision.
+
+#     Computes the precision, a metric for multi-label classification of
+#     how many selected items are relevant.
+#     """
+#     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+#     predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+#     precision = true_positives / (predicted_positives + K.epsilon())
+#     return precision
+
+
+# def recall(y_true, y_pred):
+#     """Recall metric.
+
+#     Only computes a batch-wise average of recall.
+
+#     Computes the recall, a metric for multi-label classification of
+#     how many relevant items are selected.
+#     """
+#     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+#     possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+#     recall = true_positives / (possible_positives + K.epsilon())
+#     return recall
+
 
 # Create Generators
 
-# In[17]:
+# In[8]:
 
 datagen = ImageDataGenerator(
         rescale=1. / 255,
@@ -81,14 +108,14 @@ test_generator = datagen.flow_from_directory(
 
 # Load Standard Xception Model
 
-# In[18]:
+# In[9]:
 
 model = applications.xception.Xception(include_top=False, weights='imagenet')
 
 
 # Build My "Custom" Model
 
-# In[19]:
+# In[10]:
 
 watch_model = Sequential()
 watch_model.add(model)
@@ -99,7 +126,7 @@ watch_model.summary()
 
 # Freeze Exception Layers
 
-# In[20]:
+# In[11]:
 
 for layer in watch_model.layers[0].layers:
     layer.trainable = False
@@ -107,22 +134,22 @@ for layer in watch_model.layers[0].layers:
 
 # Compile
 
-# In[21]:
+# In[12]:
 
 watch_model.compile(
     loss = "binary_crossentropy", 
     optimizer='sgd', 
-    metrics=["accuracy"])
+    metrics=["binary_accuracy"])
 
 
-# In[ ]:
+# In[13]:
 
 #Fit 
 
-epochs = 10
+epochs = 5
 
 cb1 = [callbacks.ModelCheckpoint(
-    'xception_binary_convos1_best.h5',
+    'xception_binary_gender_masked_best1.h5',
     monitor='val_loss',
     verbose=0, 
     save_best_only=True, 
@@ -141,27 +168,41 @@ watch_model.fit_generator(
         validation_steps=nb_validation_samples // batch_size,
         callbacks = cb1)
 
-watch_model.save('xception_binary.h5')
+watch_model.save('xception_binary_gender_masked_final1.h5')
+
+
+# In[14]:
+
+del watch_model
+
+
+# In[15]:
+
+watch_model = load_model('xception_binary_gender_masked_final1.h5')
+
+
+# In[16]:
 
 #Unfreeze a few more layers and allow to run
 
-watch_model.layers[0].summary()
+# watch_model.layers[0].summary()
 
 # Freeze convolutional layers
 for layer in watch_model.layers[0].layers[-3:]:
     layer.trainable = True
 
-epochs = 50
+epochs = 20
 
 
 watch_model.compile(
     loss = "binary_crossentropy", 
     optimizer='sgd', 
-    metrics=["accuracy"])
+    metrics=["binary_accuracy"])
 
+# watch_model.layers[0].summary()
 
 cb2 = [callbacks.ModelCheckpoint(
-    'xception_binary_unfrozen_convos1_best.h5',
+    'xception_binary_gender_masked_best2.h5',
     monitor='val_loss',
     verbose=0, 
     save_best_only=True, 
@@ -178,7 +219,17 @@ watch_model.fit_generator(
         callbacks = cb2)
 
 
-watch_model.save('xception_binary_unfrozen_convos1_final.h5')
+watch_model.save('xception_binary_gender_masked_final2.h5')
+
+
+# In[17]:
+
+#del watch_model
+
+
+# In[18]:
+
+#watch_model = load_model('xception_binary_unfrozen_convos1_test.h5')
 
 
 # In[ ]:
