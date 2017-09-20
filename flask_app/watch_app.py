3bd48b115ef78719a31123d5e421ebab031cd9b7
Ubuntu <ubuntu@ip-172-31-10-67.us-west-1.compute.internal>
2017-09-20 21:33:43 +0000

    final prototype of webpage running

--- flask_app/watch_app.py
+++ flask_app/watch_app.py
@@ -13,6 +13,8 @@ from keras.applications.inception_v3 import InceptionV3
 
 #---------- OPEN DATA -----------------------#
 
+
+#### Recommendation Block
 # Data for Pre-Computed Model Feature Vectors
 watch_names = pd.read_csv('inceptionv3_raw_feature_vectors_names.csv',index_col=0)
 watch_names['file_name'] = [x.split('/')[-1].replace('.jpg','')  for x in watch_names['file_name']]
@@ -25,8 +27,16 @@ watch_df = pd.read_csv('all_watch_info_with_indicators.csv')
 img_url_dict = dict(zip(watch_df.image_name, watch_df.image_url))
 url_dict = dict(zip(watch_df.image_name, watch_df.url))
 
+
 #---------- OPEN Model ----------------------#
+#### Recommendation Model
 watch_model = load_model('inceptionv3_raw_2048.h5')
+#### Banner Ad Models
+print('Woo!')
+gender_model = load_model('xception_binary_gender_masked_final2.h5')
+print('Double Woo!')
+gold_model = load_model('xception_binary_gold_masked_best3.h5')
+print('Triple Woo!')
 
 #---------- Helper Functions  ---------------#
 def prepare_image(image_name):
@@ -70,6 +80,28 @@ def make_rec(img,n_recs):
     return recommend_names
     
 
+def pred_gender(img):
+    is_male = gender_model.predict(img)
+    
+    if is_male >= 0.5:
+        gender = 'male'
+    else:
+        gender = 'female'
+    
+    return gender
+
+
+def pred_gold(img):
+    is_gold = gold_model.predict(img)
+    
+    if is_gold <= 0.5:
+        metal = 'gold'
+    else:
+        metal = 'not_gold'
+        
+    print(is_gold)
+    return metal
+
 
 #---------- URLS AND WEB PAGES -------------#
 UPLOAD_FOLDER = 'uploads'
@@ -88,21 +120,36 @@ def upload_file():
     if request.method == 'POST':
         file = request.files['file']
         if file and allowed_file(file.filename):
-
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             
             global uploaded_files
             uploaded_files.append(filename)
             
+            global img
             img = prepare_image('uploads/'+str(filename)) 
             n_recs = 8
-            
             recommend_names = make_rec(img,n_recs)
-
+            
+            
+            # Banner
+            gend = pred_gender(img)
+            metal = pred_gold(img)
+            
+            if gend == 'male' and metal == 'gold':
+                banner = 'http://127.0.0.1:5000/static/james_bond_tux_cropped.jpg'
+            elif gend == 'male' and metal == 'not_gold':
+                banner = 'http://127.0.0.1:5000/static/paul_newman_racecar_cropped2.jpg'
+            elif gend == 'female' and metal == 'gold':
+                banner = 'http://127.0.0.1:5000/static/audrey_hepburn_banner2.jpg'
+            else:
+                banner = 'http://127.0.0.1:5000/static/jane_fonda_cropped.jpg'
+              
             #global rec_list
             global rec_dict
             rec_dict = dict()
+            global banner_name
+            banner_name = banner
             
             rec_list = []
             for rec in recommend_names:
@@ -121,12 +168,25 @@ def uploaded_file(filename):
     
     if filename in rec_dict:
         rec_list=rec_dict[filename]
+        banner = banner_name
     else:
         # If pred doesn't exist make new pred
         img = prepare_image(
             'static/images/'+filename+'.jpg') 
         n_recs = 8
         recommend_names = make_rec(img,n_recs)
+        gend = pred_gender(img)
+        metal = pred_gold(img)
+        
+        if gend == 'male' and metal == 'gold':
+            banner = 'http://127.0.0.1:5000/static/james_bond_tux_cropped.jpg'
+        elif gend == 'male' and metal == 'not_gold':
+            banner = 'http://127.0.0.1:5000/static/paul_newman_racecar_cropped2.jpg'
+        elif gend == 'female' and metal == 'gold':
+            banner = 'http://127.0.0.1:5000/static/audrey_hepburn_banner2.jpg'
+        else:
+            banner = 'http://127.0.0.1:5000/static/jane_fonda_cropped.jpg'
+              
         rec_list = []
         for rec in recommend_names:
             rec_list.append(
@@ -134,13 +194,18 @@ def uploaded_file(filename):
                  'http://127.0.0.1:5000/static/images/'+rec+'.jpg',
                  url_dict[rec]])
         rec_dict[filename] = rec_list
+        
     
     # Check if Uploaded Files
     if filename in uploaded_files:
-        filename = 'http://127.0.0.1:5000/uploads/' + filename
+        filename = 'http://127.0.0.1:5000/uploads/' + filename    
     else:    
         filename = 'http://127.0.0.1:5000/static/images/'+ filename + '.jpg'
-    return render_template('watch_page.html', filename=filename,rec_list=rec_list)
+     
+    return render_template('watch_page.html',
+                           filename=filename,
+                           rec_list=rec_list,
+                           ban=banner)
 
 @app.route('/uploads/<filename>')
 def send_file(filename):
